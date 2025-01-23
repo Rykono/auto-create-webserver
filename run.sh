3cde2ec489c44ab8047eb9ee8dd799a81448adff
Rykono <Rykono99@gmail.com>
2025-01-23 17:14:40 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -32,7 +32,7 @@ run_with_progress "sudo apt update" "Updating package lists"
 
 # Install PHP 8.2 and extensions
 echo "Installing PHP 8.2 and required extensions..."
-run_with_progress "sudo apt install -y php8.2 php8.2-bcmath php8.2-ctype php8.2-json php8.2-xml php8.2-pdo php8.2-mbstring php8.2-curl php8.2-gd php8.2-imagick php8.2-zip" "Installing PHP 8.2 and extensions"
+run_with_progress "sudo apt install -y php8.2 php8.2-bcmath php8.2-ctype php8.2-xml php8.2-pdo php8.2-mbstring php8.2-curl php8.2-gd php8.2-imagick php8.2-zip" "Installing PHP 8.2 and extensions"
 
 # Install other necessary dependencies
 run_with_progress "sudo apt install -y apache2 mysql-server redis composer curl build-essential python3" "Installing other dependencies"
