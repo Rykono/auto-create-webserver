a9d63b69fa91ea2c1ab380bb5e44f7a77493dd77
Rykono <Rykono99@gmail.com>
2025-01-23 15:00:01 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,27 +5,44 @@ read -p "Github Repository (xxxxx.git): " githubres
 sudo apt update && sudo apt upgrade -y
 sudo apt install apache2 mysql-server php php-bcmath php-ctype php-json php-xml php-pdo php-mbstring php-curl php-gd php-imagick php-zip redis composer -y
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+sleep 5
 export NVM_DIR="$HOME/.nvm" 
+sleep 1
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+sleep 1
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+sleep 1
 source ~/.bashrc
+sleep 1
 if test -f "/etc/apache2/sites-available/$domainname"; then
     echo "$domainname exists. Deleting and recreating"
     rm -R /etc/apache2/sites-available/$domainname
 fi
-touch /etc/apache2/sites-available/$domainname.conf
-echo -e "<VirtualHost *:80>\r\n        ServerName $domainname.com\r\n        ServerAlias www.$domainname.com\r\n        ServerAdmin webmaster@localhost\r\n        DocumentRoot /var/www/html/$domainname.com/public\r\n        #Redirect / https://$domainname.com #uncomment this when ssl active or live \r\n\r\n        <Directory \"/var/www/html/$domainname.com/public/\">\r\n                Options  FollowSymLinks\r\n                AllowOverride All\r\n                Order Allow,Deny\r\n                Allow from all\r\n        </Directory>\r\n\r\n        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,\r\n        # error, crit, alert, emerg.\r\n        # It is also possible to configure the loglevel for particular\r\n        # modules, e.g.\r\n        #LogLevel info ssl:warn\r\n\r\n        ErrorLog ${APACHE_LOG_DIR}/error.log\r\n        CustomLog ${APACHE_LOG_DIR}/access.log combined\r\n\r\n        # For most configuration files from conf-available/, which are\r\n        # enabled or disabled at a global level, it is possible to\r\n        # include a line for only one particular virtual host. For example the\r\n        # following line enables the CGI configuration for this host only\r\n        # after it has been globally disabled with \"a2disconf\".\r\n        #Include conf-available/serve-cgi-bin.conf\r\n</VirtualHost>\r\n<IfModule mod_ssl.c>\r\n        <VirtualHost *:443>\r\n                ServerAdmin webmaster@localhost\r\n                ServerName $domainname.com\r\n                DocumentRoot /var/www/html/$domainname.com/public\r\n\r\n                <Directory />\r\n                        Order Deny,Allow\r\n                        Deny from all\r\n                        Options None\r\n                        AllowOverride None\r\n                </Directory>\r\n\r\n                <Directory /var/www/html/$domainname.com/public>\r\n                        Options FollowSymLinks\r\n                        AllowOverride All\r\n                        Order Allow,Deny\r\n                        Allow from all\r\n                </Directory>\r\n\r\n\r\n                ErrorLog ${APACHE_LOG_DIR}/$domainname/error.log\r\n                CustomLog ${APACHE_LOG_DIR}/$domainname/access.log combined\r\n\r\n                SSLEngine on\r\n                SSLProtocol all\r\n                SSLCertificateFile /etc/apache2/ssl/$domainname.com.pem\r\n                SSLCertificateKeyFile /etc/apache2/ssl/$domainname.com.key\r\n\r\n                <FilesMatch \"\\.(cgi|shtml|phtml|php)$\">\r\n                                SSLOptions +StdEnvVars\r\n                </FilesMatch>\r\n                <Directory /usr/lib/cgi-bin>\r\n                                SSLOptions +StdEnvVars\r\n                </Directory>\r\n\r\n        </VirtualHost>\r\n</IfModule>" >> /etc/apache2/sites-available/$domainname.conf
+sudo touch /etc/apache2/sites-available/$domainname.conf
+sudo echo -e "<VirtualHost *:80>\r\n        ServerName $domainname.com\r\n        ServerAlias www.$domainname.com\r\n        ServerAdmin webmaster@localhost\r\n        DocumentRoot /var/www/html/$domainname.com/public\r\n        #Redirect / https://$domainname.com #uncomment this when ssl active or live \r\n\r\n        <Directory \"/var/www/html/$domainname.com/public/\">\r\n                Options  FollowSymLinks\r\n                AllowOverride All\r\n                Order Allow,Deny\r\n                Allow from all\r\n        </Directory>\r\n\r\n        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,\r\n        # error, crit, alert, emerg.\r\n        # It is also possible to configure the loglevel for particular\r\n        # modules, e.g.\r\n        #LogLevel info ssl:warn\r\n\r\n        ErrorLog ${APACHE_LOG_DIR}/error.log\r\n        CustomLog ${APACHE_LOG_DIR}/access.log combined\r\n\r\n        # For most configuration files from conf-available/, which are\r\n        # enabled or disabled at a global level, it is possible to\r\n        # include a line for only one particular virtual host. For example the\r\n        # following line enables the CGI configuration for this host only\r\n        # after it has been globally disabled with \"a2disconf\".\r\n        #Include conf-available/serve-cgi-bin.conf\r\n</VirtualHost>\r\n<IfModule mod_ssl.c>\r\n        <VirtualHost *:443>\r\n                ServerAdmin webmaster@localhost\r\n                ServerName $domainname.com\r\n                DocumentRoot /var/www/html/$domainname.com/public\r\n\r\n                <Directory />\r\n                        Order Deny,Allow\r\n                        Deny from all\r\n                        Options None\r\n                        AllowOverride None\r\n                </Directory>\r\n\r\n                <Directory /var/www/html/$domainname.com/public>\r\n                        Options FollowSymLinks\r\n                        AllowOverride All\r\n                        Order Allow,Deny\r\n                        Allow from all\r\n                </Directory>\r\n\r\n\r\n                ErrorLog ${APACHE_LOG_DIR}/$domainname/error.log\r\n                CustomLog ${APACHE_LOG_DIR}/$domainname/access.log combined\r\n\r\n                SSLEngine on\r\n                SSLProtocol all\r\n                SSLCertificateFile /etc/apache2/ssl/$domainname.com.pem\r\n                SSLCertificateKeyFile /etc/apache2/ssl/$domainname.com.key\r\n\r\n                <FilesMatch \"\\.(cgi|shtml|phtml|php)$\">\r\n                                SSLOptions +StdEnvVars\r\n                </FilesMatch>\r\n                <Directory /usr/lib/cgi-bin>\r\n                                SSLOptions +StdEnvVars\r\n                </Directory>\r\n\r\n        </VirtualHost>\r\n</IfModule>" >> /etc/apache2/sites-available/$domainname.conf
+sleep 1
 sudo rm /etc/apache2/sites-available/000-default.conf
+sleep 1
 sudo rm /etc/apache2/sites-available/default-ssl.conf
+sleep 1
 sudo a2dissite 000-default.conf
+sleep 1
 sudo a2enmod rewrite
+sleep 1
 sudo a2ensite $domainname.conf
+sleep 1
 sudo service apache2 restart
-mkdir /var/www/html/$domainname.com
+sleep 1
+sudo mkdir /var/www/html/$domainname.com
+sleep 1
+sudo chown -R ubuntu:www-data /var/www/html/$domainname.com
+sleep 1
 cd /var/www/html/$domainname.com
+sleep 1
 git init
 git remote add origin https://$githubuser:$githubtoken@github.com/$githubuser/$githubres
 git pull origin master
+sleep 1
 sudo usermod -a -G www-data ubuntu
 sudo chown -R ubuntu:www-data .
 sudo find . -type f -exec chmod 664 {} \;   
