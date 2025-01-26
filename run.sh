962148066a6fbe365f8246d1157e16c4bb56c15b
Rykono <Rykono99@gmail.com>
2025-01-26 11:31:09 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -32,7 +32,7 @@ run_with_progress "sudo apt update" "Updating package lists"
 
 # Install PHP 8.2 and extensions
 echo "Installing PHP 8.2 and required extensions..."
-run_with_progress "sudo apt install -y php8.2 php8.2-bcmath php8.2-ctype php8.2-xml php8.2-pdo php8.2-mbstring php8.2-curl php8.2-gd php8.2-imagick php8.2-zip php8.2-intl php8.2-dev" "Installing PHP 8.2 and extensions"
+run_with_progress "sudo apt install -y php8.2 php8.2-bcmath php8.2-ctype php8.2-mysql php8.2-xml php8.2-pdo php8.2-mbstring php8.2-curl php8.2-gd php8.2-imagick php8.2-zip php8.2-intl php8.2-dev" "Installing PHP 8.2 and extensions"
 
 # Set PHP 8.2 as the default version
 echo "Setting PHP 8.2 as the default version..."
