3e5b72aa80e5e4f5a3e7ae476752a23bbf5ce4d0
Rykono <Rykono99@gmail.com>
2025-01-26 11:22:27 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -32,14 +32,23 @@ run_with_progress "sudo apt update" "Updating package lists"
 
 # Install PHP 8.2 and extensions
 echo "Installing PHP 8.2 and required extensions..."
-run_with_progress "sudo apt install -y php8.2 php8.2-bcmath php8.2-ctype php8.2-xml php8.2-pdo php8.2-mbstring php8.2-curl php8.2-gd php8.2-imagick php8.2-zip php8.2-intl" "Installing PHP 8.2 and extensions"
+run_with_progress "sudo apt install -y php8.2 php8.2-bcmath php8.2-ctype php8.2-xml php8.2-pdo php8.2-mbstring php8.2-curl php8.2-gd php8.2-imagick php8.2-zip php8.2-intl php8.2-dev" "Installing PHP 8.2 and extensions"
 
 # Set PHP 8.2 as the default version
 echo "Setting PHP 8.2 as the default version..."
 run_with_progress "sudo update-alternatives --set php /usr/bin/php8.2" "Setting PHP 8.2 as default"
 
 # Install other necessary dependencies
-run_with_progress "sudo apt install -y apache2 mysql-server redis composer curl build-essential python3" "Installing other dependencies"
+run_with_progress "sudo apt install -y apache2 mysql-server redis-server composer curl build-essential python3" "Installing other dependencies"
+
+# Install Redis extension for PHP 8.2 using PECL
+echo "Installing Redis extension for PHP 8.2..."
+run_with_progress "yes '' | sudo pecl install redis" "Installing Redis extension via PECL"
+
+# Enable the Redis extension in PHP
+echo "Enabling Redis extension in PHP..."
+run_with_progress "echo 'extension=redis.so' | sudo tee /etc/php/8.2/mods-available/redis.ini" "Creating Redis configuration file"
+run_with_progress "sudo phpenmod redis" "Enabling Redis extension"
 
 # Install NVM
 echo "Installing NVM..."
@@ -47,14 +56,6 @@ run_with_progress "curl https://raw.githubusercontent.com/creationix/nvm/master/
 export NVM_DIR="$HOME/.nvm"
 source "$NVM_DIR/nvm.sh"
 source "$NVM_DIR/bash_completion"
-
-# Check and handle 000-default.conf
-echo "Configuring Apache..."
-if sudo a2dissite 000-default.conf &> /dev/null; then
-    echo -e "[Disabling default site] \e[32mDone\e[0m" # Green "Done"
-else
-    echo -e "[Disabling default site] \e[33mSkipped (already disabled or not found)\e[0m" # Yellow "Skipped"
-fi
 
 # Check and handle existing project directory
 project_dir="/var/www/html/$domainname.com"
@@ -92,9 +93,64 @@ run_with_progress "sudo chmod -R ug+rwx storage bootstrap/cache" "Setting permis
 echo "Installing project dependencies..."
 echo "Running Composer install..."
 composer install
+
+# Node & NPM install
 echo "Running NVM setup and NPM install..."
 nvm install --lts
 nvm use --lts
 npm install
+
+###############################################################################
+#                  APACHE VIRTUALHOST CONFIGURATION SNIPPET                   #
+###############################################################################
+echo "Configuring Apache..."
+config_path="/etc/apache2/sites-available/${domainname}.conf"
+
+# Write out the VirtualHost configuration
+sudo tee "$config_path" > /dev/null <<EOF
+<VirtualHost *:80>
+    ServerName $domainname.com
+    ServerAlias www.$domainname.com
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/html/$domainname.com/public
+    #Redirect / https://$domainname.com # Uncomment when SSL is active
+    <Directory "/var/www/html/$domainname.com/public/">
+        Options FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+
+<IfModule mod_ssl.c>
+    <VirtualHost *:443>
+        ServerName $domainname.com
+        DocumentRoot /var/www/html/$domainname.com/public
+        <Directory />
+            Require all denied
+            Options None
+            AllowOverride None
+        </Directory>
+        <Directory "/var/www/html/$domainname.com/public/">
+            Options FollowSymLinks
+            AllowOverride All
+            Require all granted
+        </Directory>
+        ErrorLog \${APACHE_LOG_DIR}/$domainname/error.log
+        CustomLog \${APACHE_LOG_DIR}/$domainname/access.log combined
+        SSLEngine on
+        SSLProtocol all
+        SSLCertificateFile /etc/apache2/ssl/$domainname.com.pem
+        SSLCertificateKeyFile /etc/apache2/ssl/$domainname.com.key
+    </VirtualHost>
+</IfModule>
+EOF
+
+# Enable Apache configuration
+run_with_progress "sudo a2dissite 000-default.conf" "Disabling default site"
+run_with_progress "sudo a2enmod rewrite" "Enabling mod_rewrite"
+run_with_progress "sudo a2ensite ${domainname}.conf" "Enabling site configuration"
+run_with_progress "sudo service apache2 restart" "Restarting Apache"
 
 echo -e "\n\e[32mSetup complete! Configure Vite and then npm run build.\e[0m"
